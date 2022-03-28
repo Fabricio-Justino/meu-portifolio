729b1c39ab3599bb14ce4d72df55b7ef181452ea
Fabricio-Justino <fabriciobj667@gmail.com>
2022-03-28 00:50:39 -0300

    tornando jogo da cobrinha dinâmico, para jogar no mobile

--- js/Snake-game/main.js
+++ js/Snake-game/main.js
@@ -10,30 +10,35 @@ import {
     ControlerMobile
 } from "../controleMobile.js";
 
-const GAME = new DrawWork(600, 400, 'canvas');
-const WIDTH = 10;
-const $div = document.getElementById('game-over');
+
+const $container = document.querySelectorAll('.container');
+const GAME = new DrawWork($container[0].clientWidth, $container[0].clientHeight, 'canvas');
+const $div = document.querySelector('#game-over');
 $div.classList.add('game-over');
 
-let color = 'red';
-let grid = new Array(400 / WIDTH).fill(null).map(el => new Array(600 / WIDTH).fill(0));
+let resolution = (GAME.width / 100) * 2;
+let gridRow = Math.floor(GAME.height / resolution);
+let gridCol = Math.floor(GAME.width / resolution);
+let grid = new Array(gridRow).fill(null).map(el => new Array(gridCol).fill(0));
 let snake = new Snake(9, 14, grid);
+
+let color = 'red';
 let thereIsfruitInGame = false;
 let isGameOver
 let score = 0;
 
-const controler = new ControlerMobile(document.getElementById('controler-container'));
+const controler = new ControlerMobile(document.querySelector('.controller'));
 
 function makeFruit() {
-    if(!thereIsfruitInGame) {
-        let row = DrawWork.random(0, grid.length-1);
-        let col = DrawWork.random(0, grid[0].length-1);
-        
-        while(grid[row][col] !== 0) {
-            row = DrawWork.random(0, grid.length-1); 
-            col = DrawWork.random(0, grid[0].length-1); 
+    if (!thereIsfruitInGame) {
+        let row = DrawWork.random(0, grid.length - 1);
+        let col = DrawWork.random(0, grid[0].length - 1);
+
+        while (grid[row][col] !== 0) {
+            row = DrawWork.random(0, grid.length - 1);
+            col = DrawWork.random(0, grid[0].length - 1);
         }
-        
+
         grid[row][col] = 2;
         thereIsfruitInGame = true;
         snake.putFruit(row, col);
@@ -42,6 +47,7 @@ function makeFruit() {
 }
 
 let frameCount = 1;
+
 function loop() {
     $div.innerHTML = `pontos: ${score}`;
     GAME.backGround(0, 0, 0);
@@ -68,11 +74,11 @@ function renderGrid(grid) {
     grid.forEach((array, row) => {
         array.forEach((el, col) => {
             if (el === 2) {
-                GAME.fillRect(col * WIDTH, row * WIDTH, WIDTH, WIDTH, color);
+                GAME.fillRect(col * resolution, row * resolution, resolution, resolution, color);
             }
 
             if (el === 1) {
-                GAME.fillRect(col * WIDTH, row * WIDTH, WIDTH, WIDTH, 'blue');
+                GAME.fillRect(col * resolution, row * resolution, resolution, resolution, 'blue');
             }
         });
     });
@@ -92,9 +98,22 @@ function scoreUp() {
 
 window.addEventListener('keydown', keydown);
 
+controler.canvas.addEventListener('click', function(e) {
+    snake.move(controler.click(e));
+});
+
 function keydown(e) {
     snake.move(e.key);
 }
 
 GAME.draw(loop);
 controler.start();
+
+window.addEventListener('resize', function (e) {
+    GAME.setDimension($container[0].clientWidth, $container[0].clientHeight);
+    let resolution = (GAME.width / 100) * 2;
+    let gridRow = Math.floor(GAME.height / resolution);
+    let gridCol = Math.floor(GAME.width / resolution);
+    let grid = new Array(gridRow).fill(null).map(el => new Array(gridCol).fill(0));
+    let snake = new Snake(9, 14, grid);
+});
--- js/controleMobile.js
+++ js/controleMobile.js
@@ -5,13 +5,20 @@ import {
 export class ControlerMobile {
 
     constructor(container) {
+        this.applyStyle(container);
         const $canvas = document.createElement('canvas');
         $canvas.id = 'controler-mobile';
         const ctx = $canvas.getContext('2d');
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, 400, 400);
         container.appendChild($canvas);
+
         this.engine = new DrawWork(container.clientWidth, container.clientHeight, 'controler-mobile');
+        this.canvas = $canvas;
+        this.ArrowUp = null;
+        this.ArrowDown = null;
+        this.ArrowLeft = null;
+        this.ArrowRight = null;
     }
 
     start() {
@@ -25,33 +32,155 @@ export class ControlerMobile {
         this.buttons(c, w, h);
     }
 
+    applyStyle(container) {
+        const c = container;
+
+        c.style.display = 'block';
+        c.style.margin = '0 auto';
+        c.style.maxWidth = '600px';
+        c.style.maxHeight = '50vh'
+        c.style.width = '100%';
+        c.style.height = '100%';
+    }
+
     pad(c, w, h) {
         const wp = this.percentToPixel(w, 10);
         const hp = this.percentToPixel(h, 10);
 
         c.fillRect(15, h / 2, 65 + wp, 25 + hp, 'white');
         c.fillRect((65 / 3) + (25 / 2), (h / 2) - 20, 25 + wp, 65 + hp, 'white');
-        c.endScale();
+
         c.fillRect(20, h / 2 + (5 / 2), 55 + wp, 20 + hp, 'grey');
         c.fillRect((65 / 3) + (30 / 2), (h / 2) - 15, 20 + wp, 55 + hp, 'grey');
+
+        this.ArrowUp = (e) => {
+            const MOUSE_X = e.pageX - e.target.offsetLeft;
+            const MOUSE_Y = e.pageY - e.target.offsetTop;
+
+            const x = (65 / 3) + (30 / 2);
+            const y = (h / 2) - 15;
+            const width = 20 + wp;
+            const height = 55 + hp;
+
+            // C = COLISION
+            const C_LEFT = MOUSE_X > x;
+            const C_RIGHT = MOUSE_X < x + width;
+            const C_UP = MOUSE_Y > y;
+            const C_DOWN = MOUSE_Y < y + height / 3;
+
+            if (C_DOWN && C_RIGHT && C_UP && C_LEFT) {
+                return true;
+            } else {
+                return false;
+            }
+
+        };
+
+        this.ArrowDown = (e) => {
+            const MOUSE_X = e.pageX - e.target.offsetLeft;
+            const MOUSE_Y = e.pageY - e.target.offsetTop;
+
+            const x = (65 / 3) + (30 / 2);
+            const y = (h / 2) - 15;
+            const width = 20 + wp;
+            const height = 55 + hp;
+
+            // C = COLISION
+            const C_LEFT = MOUSE_X > x;
+            const C_RIGHT = MOUSE_X < x + width;
+            const C_UP = MOUSE_Y > y * 2;
+            const C_DOWN = MOUSE_Y < (y * 2) + height;
+
+            if (C_DOWN && C_RIGHT && C_UP && C_LEFT) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        this.ArrowLeft = (e) => {
+            const MOUSE_X = e.pageX - e.target.offsetLeft;
+            const MOUSE_Y = e.pageY - e.target.offsetTop;
+
+            const x = 20;
+            const y = h / 2 + (5 / 2);
+            const width = 55 + wp;
+            const height = 20 + hp;
+
+            // C = COLISION
+            const C_LEFT = MOUSE_X > x;
+            const C_RIGHT = MOUSE_X < x + width / 4;
+            const C_UP = MOUSE_Y > y;
+            const C_DOWN = MOUSE_Y < y + height;
+
+
+            if (C_DOWN && C_RIGHT && C_UP && C_LEFT) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        this.ArrowRight = (e) => {
+            const MOUSE_X = e.pageX - e.target.offsetLeft;
+            const MOUSE_Y = e.pageY - e.target.offsetTop;
+
+            const x = 20;
+            const y = h / 2 + (5 / 2);
+            const width = 55 + wp;
+            const height = 20 + hp;
+
+            // C = COLISION
+            const C_LEFT = MOUSE_X > x * 5;
+            const C_RIGHT = MOUSE_X < x * 5 + width;
+            const C_UP = MOUSE_Y > y;
+            const C_DOWN = MOUSE_Y < y + height;
+
+
+            if (C_DOWN && C_RIGHT && C_UP && C_LEFT) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
     }
 
     buttons(c, w, h) {
 
         // r = radius
         const r = 25;
-        const marginX = 180;
-        const marginY = 165;
+        const marginX = this.percentToPixel(w, 40);
+        const marginY = this.percentToPixel(h, 40);
 
         c.fillRect(w - marginX, h - marginY, r * 2, r * 2, 'white');
         c.dot(w - (marginX - r), h - (marginY - r), r, 'grey');
 
-        c.fillRect(w - marginX + r*4, h - marginY, r * 2, r * 2, 'white');
-        c.dot(w - (marginX - r) + r*4, h - (marginY - r), r, 'grey');
+        c.fillRect(w - marginX + r * 4, h - marginY, r * 2, r * 2, 'white');
+        c.dot(w - (marginX - r) + r * 4, h - (marginY - r), r, 'grey');
     }
 
     strat() {
 
+    }
+
+    click(e) {
+
+        if (this.ArrowLeft(e)) {
+            return "ArrowLeft"
+        }
+
+        if (this.ArrowRight(e)) {
+            return "ArrowRight";
+        }
+
+        if (this.ArrowUp(e)) {
+            return "ArrowUp"
+        }
+
+        if (this.ArrowDown(e)) {
+            return "ArrowDown";
+        }
     }
 
     percentToPixel(maxPixel, percent) {
